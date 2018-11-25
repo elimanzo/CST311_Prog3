dfd1abf5722cebd07001d4b34fdeed57d36de537
elmanzo <elmanzo@scsumb.edu>
2018-11-24 17:38:52 -0800

    Debugging Legacy Router

--- legacy_router.py
+++ legacy_router.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python
 
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSController
-from mininet.node import CPULimitedHost, Host, Node
-from mininet.node import OVSKernelSwitch, UserSwitch
-from mininet.node import IVSSwitch
+from mininet.node import Host, Node
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-from mininet.link import TCLink, Intf
-from subprocess import call
 
 def myNetwork():
-
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
-
-    info( '*** Adding controller\n' )
-    info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    defaultIP = '10.0.1.1/24' # IP addr for r1-eth0
+    net = Mininet( topo=None, build=False, ipBase=defaultIP)
+	
+    r1 = net.addHost('r1', cls=Node, ip=defaultIP)
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
-
+				  
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
 
     info( '*** Add links\n')
-    net.addLink(r1, h1)
-    net.addLink(r1, h2)
+    net.addLink(h1, r1, intfName2='r1-eth0', params2={ 'ip' : defaultIP } )
+    net.addLink(h2, r1, intfName2='r1-eth1', params2={ 'ip' : '10.0.2.1/24' } )
 
     info( '*** Starting network\n')
     net.build()
-    info( '*** Starting controllers\n')
-    for controller in net.controllers:
-        controller.start()
 
-    info( '*** Starting switches\n')
-
-    info( '*** Post configure switches and hosts\n')
-
+    info( '*** Routing Table on Router:\n' )
+    print net[ 'r1' ].cmd( 'route' )
     CLI(net)
     net.stop()
 
